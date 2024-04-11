26a2b542d28f9db91923d48fe7e04f4cd8de9686
Muskan-Thakur <106293646+Muskan-Thakur@users.noreply.github.com>
2024-04-11 14:30:37 +0530

    Add files via upload

---
+++ second.js
@@ -0,0 +1,39 @@
+let a = document.querySelector("#main .card-img-top");
+let b = document.getElementById('firstpara');
+
+
+
+function first(){
+    a.src = "img/g.jpg"
+    b.innerHTML = "Unabated Stress/Lack of Sleep/Poor Nutrition"
+}
+function second()
+{
+    a.src = "img/b.jpg"
+    b.innerHTML = "Hypertension/Diabetes Mellitus /Polycystic Kidney Disease"
+}
+function third()
+{
+    a.src = "img/c.jpg"
+    b.innerHTML = "Unabated Stress / Lack of Sleep  / Poor Nutrition"
+}
+function four()
+{
+    a.src = "img/d.jpg"
+    b.innerHTML = "Fatigue / Low Libido / Depression / Decreased Energy / Hair Loss 4"
+}
+function five()
+{
+    a.src = "img/e.jpg"
+    b.innerHTML = "Poor circulation / Chronic Headaches / Lethargy / Signs of Aging"
+}
+function six()
+{
+    a.src = "img/f.jpg"
+    b.innerHTML = "Unexplained weight gain / Hypothyroidism / Slow metabolism"
+}
+function seven()
+{
+    a.src = "img/a.jpg"
+    b.innerHTML = "Botox / Fillers /PRP Microneedling / PDO Thread Lifts /"
+}
